afb9eae59e4f76a7b36ec5f9841d5bf183ee7378
Romulo Vieira <romulo.vieira777@gmail.com>
2022-10-31 10:11:17 -0300

    Starter

---
+++ Section_02_Starter/01_Exercise.py
@@ -0,0 +1,17 @@
+"""
+Exercise No. 0
+
+Print the Python version to the console.
+
+Expected result:
+
+    3.11.0
+"""
+import sys
+
+# Solution I
+print(sys.version[0:6])
+
+
+# Solution II
+print(sys.version.split()[0])
