aad5bf870c28d50c0e81d54c1c6c6e4b2ff1cda2
Romulo Vieira <romulo.vieira777@gmail.com>
2022-11-05 09:53:07 -0300

    Data Types

---
+++ Section_07_Data_Types/01_Exercise.py
@@ -0,0 +1,24 @@
+"""
+Exercise No. 01
+
+The following variables are given (var1 - empty string, var2 - space, var3 - new line character):
+
+    var1 = ''
+    var2 = ' '
+    var3 = '\n'
+
+Print each type of variable on a separate line to the console as shown below.
+
+Expected result:
+
+    <class 'str'>
+    <class 'str'>
+    <class 'str'>
+"""
+var1 = ''
+var2 = ' '
+var3 = '\n'
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
---
+++ Section_07_Data_Types/03_Exercise.py
@@ -0,0 +1,16 @@
+"""
+Exercise No. 03
+
+Check if the following variable:
+
+    flag = False
+
+Is an instance of the bool class and print the result to the console.
+
+Expected result:
+
+    True
+"""
+flag = False
+
+print(isinstance(flag, bool))
