2a60b69c3b894f14cb14a11623398041799deebe
Romulo Vieira <romulo.vieira777@gmail.com>
2022-11-11 11:31:59 -0300

    Tuples

---
+++ Section_11_Tuples/01_Exercise.py
@@ -0,0 +1,19 @@
+"""
+Exercise No. 01
+
+Two following tuples are given:
+
+    dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+    dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+Combine these tuples into one as shown below and print the result to the console.
+
+Expected result:
+
+    ('AAPL.US', 'IBM.US', 'MSFT.US', 'HD.US', 'GS.US', 'NKE.US')
+"""
+dji1 = ('AAPL.US', 'IBM.US', 'MSFT.US')
+dji2 = ('HD.US', 'GS.US', 'NKE.US')
+
+dj = dji1 + dji2
+print(dj)
