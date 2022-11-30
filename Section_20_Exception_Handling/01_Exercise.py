8879c130732e03af520cc8d53dff9e38bf6dfdaf
Romulo Vieira <romulo.vieira777@gmail.com>
2022-11-30 09:19:13 -0300

    Exception Handling

---
+++ Section_20_Exception_Handling/01_Exercise.py
@@ -0,0 +1,22 @@
+"""
+Exercise No. 01
+
+Two variables defined below are given:
+
+    sum = 3000
+    counter = 0
+
+We want to divide the variable sum by the counter variable. Using the try... except... clause handle the
+ZeroDivisionError. If the division is done correctly, print the result to the console, otherwise print to the console
+the following message:
+
+    "Division by zero."
+"""
+sum = 3000
+counter = 0
+
+try:
+    result = sum / counter
+    print(result)
+except ZeroDivisionError:
+    print("Division by zero.")
