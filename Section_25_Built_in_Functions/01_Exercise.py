6c62072d1b5f7c17f7ec0b8eb2390fcb8f87b1b6
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-03 19:54:32 -0300

    Built-in Functions

---
+++ Section_25_Built_in_Functions/01_Exercise.py
@@ -0,0 +1,23 @@
+"""
+Exercise No. 01
+
+The variable x:
+
+    x = 1.5
+
+And the following expression are given:
+
+    expression = 'x ** 2 + x'
+
+Using the appropriate function, calculate the value of this expression and print result to the console.
+
+Tip: Use the eval() function.
+
+Expected result:
+
+    0.75
+"""
+x = -1.5
+expression = 'x ** 2 + x'
+print(eval(expression))
+
