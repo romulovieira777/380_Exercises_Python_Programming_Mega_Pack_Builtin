f56e9bf88e40ef7c99de514b66a7d86ed21a9a56
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-10 11:08:23 -0300

    Lambda Expression

--- Section_27_Lambda_Expression/01_Exercise.py
+++ Section_27_Lambda_Expression/01_Exercise.py
@@ -1,3 +1,25 @@
 """
+Exercise No.01
 
+The following list of words is given:
+
+    stocks = ['playway', 'boombit', 'cd projekt']
+
+Using the map() function and the lambda expression, transform the given list into a list containing the lengths of each
+word and print it to the console.
+
+Expected result:
+
+    [7, 7, 10]
 """
+
+# Solution I
+stocks = ['playway', 'boombit', 'cd projekt']
+
+print(list(map(lambda x: len(x), stocks)))
+
+
+# Solution II
+stocks = ['playway', 'boombit', 'cd projekt']
+length = list(map(lambda stock: len(stock), stocks))
+print(length)
