fbd0ebb3e159c9aada94f7ea965f08f468ef17d8
Romulo Vieira <romulo.vieira777@gmail.com>
2023-01-05 09:10:17 -0300

    Built-in Packages

--- Section_34_Built_in_Packages/11_Exercise.py
+++ Section_34_Built_in_Packages/11_Exercise.py
@@ -1,5 +1,30 @@
 """
 Exercise No. 11
 
+Using the random built-in module set the random seed as follows:
 
+    random.seed(12)
+
+And select randomly (pseudo-randomly) an item from the list below:
+
+    items = ['python', 'java', 'sql', 'c++', 'c']
+
+Print the result to the console.
+
+Expected result:
+
+    c++
 """
+import random
+
+items = ['python', 'java', 'sql', 'c++', 'c']
+
+# Solution I
+random.seed(12)
+print(random.choice(items))
+
+
+# Solution II
+random.seed(12)
+choice = random.choice(items)
+print(choice)
--- Section_34_Built_in_Packages/12_Exercise.py
+++ Section_34_Built_in_Packages/12_Exercise.py
@@ -1,5 +1,24 @@
 """
 Exercise No. 12
 
+Using the random built-in module set the random seed as follows:
 
+    random.seed(15)
+
+And shuffle (pseudo-randomly) items in the following list:
+
+    items = ['python', 'java', 'sql', 'c++', 'c']
+
+In response, print the list to the console.
+
+Expected result:
+
+    ['c++', 'c', 'sql', 'java', 'python']
 """
+import random
+
+items = ['python', 'java', 'sql', 'c++', 'c']
+
+random.seed(15)
+random.shuffle(items)
+print(items)
