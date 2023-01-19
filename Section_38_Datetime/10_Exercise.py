0845259edec47d467d5568a42f74360db0bb47db
Romulo Vieira <romulo.vieira777@gmail.com>
2023-01-19 09:19:35 -0300

    Datetime

---
+++ Section_38_Datetime/10_Exercise.py
@@ -0,0 +1,36 @@
+"""
+Exercise No. 10
+
+Using the built-in datetime module, the date and timedelta classes from the given date 2020-01-01 00:00:00 get the date:
+    - shifted by 7 days
+    - shifted by 30 days
+    - shifted by 30 hours
+    - shifted by 15 minutes
+
+Tip:
+    #>>> help(datetime.timedelta)
+
+    Help on class timedelta in module datetime:
+
+    class timedelta(builtins.object)
+     |  Difference between two datetime values.
+     |
+     |  timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+     |
+     |  All arguments are optional and default to 0.
+     |  Arguments may be integers or floats, and may be positive or negative.
+
+Expected result:
+    2020-01-08 00:00:00
+    2020-01-31 00:00:00
+    2020-01-02 06:00:00
+    2020-01-01 00:15:00
+"""
+import datetime
+
+date = datetime.datetime(2020, 1, 1, 0, 0, 0)
+
+print(date + datetime.timedelta(days=7))
+print(date + datetime.timedelta(days=30))
+print(date + datetime.timedelta(hours=30))
+print(date + datetime.timedelta(minutes=15))
---
+++ Section_38_Datetime/12_Exercise.py
@@ -0,0 +1,38 @@
+"""
+Exercise No. 12
+
+Using the built-in datetime module calculate the future value of the investment (present value) of USD 1000 with an
+annual interest rate of 4% (rate) and daily compound capitalization of interest, assuming the duration of the investment
+from 2021-07-01 to 2021-12-31. For calculations, assume that the year has 365 days.
+
+Print the future value to the console as shown below.
+
+Expected result:
+    Future value: $ 1020.26
+"""
+import datetime
+
+
+rate = 0.04
+pv = 1000
+
+# Solution I
+start_date = datetime.date(2021, 7, 1)
+end_date = datetime.date(2021, 12, 31)
+diff = end_date - start_date
+days = diff.days
+fv = pv * (1 + rate / 365) ** days
+print(f"Future value: $ {fv:.2f}")
+
+
+# Solution II
+rate = 0.04
+pv = 1000
+daily_rate = rate / 365.0
+
+d1 = datetime.date(2021, 7, 1)
+d2 = datetime.date(2021, 12, 31)
+duration = d2 - d1
+
+fv = pv * (1 + daily_rate) ** duration.days
+print(f'Future value: $ {fv:.2f}')
