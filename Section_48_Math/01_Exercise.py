1cd58fbfb09da9af3aae244ecdb16b2ba9fbae07
Romulo Vieira <romulo.vieira777@gmail.com>
2023-03-09 10:07:10 -0300

    Math

---
+++ Section_48_Math/01_Exercise.py
@@ -0,0 +1,19 @@
+"""
+Exercise No. 01
+
+Using the built-in math module, print two math constants to the console:
+
+    - PI number
+    - Euler's number
+
+as show below.
+
+Expected result:
+
+    Pi number: 3.141592653589793
+    Euler's number: 2.718281828459045
+"""
+import math
+
+print("Pi number:", math.pi)
+print("Euler's number:", math.e)
